65d8365a3d91319e0c733937b0c34ce39634d708
OdiSeU <secret36565@gmail.com>
2021-12-23 09:51:19 +0900

    211223
    43173 done

---
+++ js/43173.js
@@ -0,0 +1,18 @@
+// https://level.goorm.io/exam/43173/%EB%B9%84%ED%8A%B8%EC%97%B0%EC%82%B0-%EA%B8%B0%EB%B3%B8-2/quiz/1
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	let [a, b] = line.split(' ');
+	solution(a, b);
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
+
+function solution(a, b) {
+	console.log(`${a>>b}\n${a<<b}`);
+}
