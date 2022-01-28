8a5d99b337e3a16508e3b628c0bd03f4a6c23e54
OdiSeU <secret36565@gmail.com>
2022-01-28 16:24:44 +0900

    220128
    43226 done

---
+++ js/43226.js
@@ -0,0 +1,18 @@
+//https://level.goorm.io/exam/43226/16%EC%A7%84%EC%88%98/quiz/1
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	console.log(solution(+line));
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
+
+function solution(n) {
+	return n.toString(16);
+}
