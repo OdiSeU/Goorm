3c7f420bcab733c4cea9feb9b99d85366baa32de
OdiSeU <secret36565@gmail.com>
2022-01-31 18:39:29 +0900

    220130
    43091 done

---
+++ js/43091.js
@@ -0,0 +1,20 @@
+//https://level.goorm.io/exam/43091/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C-%ED%98%B8%EC%A0%9C%EB%B2%95/quiz/1
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	let [a, b] = line.split(' ').map(v=>+v);
+	console.log(solution(a, b));
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
+
+function solution(a, b) {
+	const GCD = (a, b) => b ? GCD(b, a%b) : a;
+	return GCD(a, b);
+}
