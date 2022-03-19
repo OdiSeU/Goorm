ee3d657a5c1401f0b6becc7f78e8c9632edf49f5
OdiSeU <secret36565@gmail.com>
2022-03-19 09:08:17 +0900

    220319
    43252 done

---
+++ js/43252.js
@@ -0,0 +1,14 @@
+// https://level.goorm.io/exam/43252/%EC%95%9E%EB%92%A4%EA%B0%80-%EA%B0%99%EC%9D%80-%EC%88%98/quiz/1
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	console.log(585);
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
